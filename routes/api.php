0f24d60f1ef717d3fd8ef50f5c2d5bee31409d8f
M7Tube <clashroyale.mahh@gmail.com>
2022-05-11 12:28:53 +0300

    dhhh

--- routes/api.php
+++ routes/api.php
@@ -17,40 +17,39 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-//sss
+
+
+
+//Protected Route
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::group(['as' => 'Auth.'], function () {
+        //get route
         Route::get('handover/perpage/{perpage}', [AppApiController::class, 'gethandover'])->name('gethandover');
         Route::get('inProgressInspection/history', [AppApiController::class, 'InProgressHistory'])->name('InProgressHistory');
         Route::get('ComplateInspection/history', [AppApiController::class, 'ComplateHistory'])->name('ComplateHistory');
-        // Route::get('handover/id/{id}', [AppApiController::class, 'getOnehandover'])->name('getOnehandover');
+        Route::get('homepage/perpage/{perpage}', [AppApiController::class, 'homepage'])->name('homepage');
+        // Create Route
         Route::group(['prefix' => '/create'], function () {
             Route::post('/handover', [AppApiController::class, 'handover'])->name('handover');
             Route::get('/inspection/{id}', [AppApiController::class, 'inspection'])->name('inspection');
             Route::post('/inspection/inprogress', [AppApiController::class, 'inspection_inprogress'])->name('inspection_inprogress');
             Route::post('/template', [AppApiController::class, 'template'])->name('template');
         });
-
-        Route::get('homepage/perpage/{perpage}', [AppApiController::class, 'homepage'])->name('homepage');
+        //auth route
         Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     });
-    // //
-    // Route::group(['as' => 'Auth.'], function () {
-    //     Route::apiResource('category', CategoryController::class);
-    // });
 });
+
+//Un Protected Route
 Route::group(['as' => 'noAuth.'], function () {
+    //auth route
     Route::post('login', [AuthController::class, 'login'])->name('login');
 });
-// Route::get('clear',function(){
-//     Artisan::call('config:cache');
-//     return 'sss';
-// });
 
+//this route for delete
 Route::get('/migrate', function () {
     Artisan::call('migrate:refresh');
     Artisan::call('db:seed');
@@ -70,12 +69,3 @@ Route::get('Download/Test', function () {
     //ssa
     return response()->download($file, 'filename.pdf', $headers);
 });
-
-
-
-// Route::post('t', function (Request $request) {
-//     return [
-//         'only_Example' => $request->only(['name', 'password']),
-//         'except_Example' => $request->except(['name', 'password']),
-//     ];
-// });
