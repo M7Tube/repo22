86b8c361d1ab9cf755bd3d0c2d54246a7c83d4fc
M7Tube <clashroyale.mahh@gmail.com>
2022-05-18 12:48:52 +0300

    qe

--- app/Models/InProgressInspection.php
+++ app/Models/InProgressInspection.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\ModelFilters\InspectionFilter;
+use eloquentFilter\QueryFilter\ModelFilters\Filterable;
 
 class InProgressInspection extends Model
 {
     use HasFactory;
+    use Filterable;
     use InspectionFilter;
     protected $table = 'in_progress_inspections';
     protected $primaryKey = 'IPI_id';
