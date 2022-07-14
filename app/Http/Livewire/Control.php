021580062a8583039a10ee295e60cf0b113fbfbf
M7Tube <clashroyale.mahh@gmail.com>
2022-07-14 17:11:18 +0300

    kljw

--- app/Http/Livewire/Control.php
+++ app/Http/Livewire/Control.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Attrubite;
+use App\Models\ReportCategory;
+use App\Models\Template;
+use App\Models\User;
 use Livewire\Component;
 
 class Control extends Component
 {
+    public $templateCount;
+    public $userCount;
+    public $categoryCount;
+    public $questionCount;
+
+    public function mount()
+    {
+        $this->templateCount=Template::count();
+        $this->userCount=User::count();
+        $this->categoryCount=ReportCategory::count();
+        $this->questionCount=Attrubite::count();
+    }
     public function render()
     {
         return view('livewire.control');
