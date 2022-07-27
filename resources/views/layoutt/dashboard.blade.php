f78b05a3f85678235f94a223c6e72dac7180400f
M7Tube <clashroyale.mahh@gmail.com>
2022-07-27 13:00:22 +0300

    qwe

--- resources/views/layoutt/dashboard.blade.php
+++ resources/views/layoutt/dashboard.blade.php
@@ -19,7 +19,7 @@
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-    <!-- CSS Files -->
+    <!-- CSS Filesd -->
     <link id="pagestyle" href="{{ asset('css/material-dashboard.css') }}" ?v=3.0.2 rel="stylesheet" />
     @yield('head')
     @livewireStyles
@@ -29,15 +29,6 @@
     <aside
         class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
         id="sidenav-main">
-        {{-- <div class="sidenav-header">
-            <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-                aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
-                target="_blank">
-                <img src="{{ asset('images/logo.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold text-white">{{ __('Reporty') }}</span>
-            </a>
-        </div> --}}
         <hr class="horizontal light mt-0 mb-2">
         <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
             <ul class="navbar-nav">
